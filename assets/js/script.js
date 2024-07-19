7e458393685856d6519148375315d2cf9b31c15b
pcrainer38 <pammie_78704@yahoo.com>
2024-07-19 17:47:59 -0500

    added dark mode and toggle switch

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const themeSwitcher = document.getElementById("theme-switcher");
+const main = document.querySelector(".main");
+
+let mode = "light";
+
+themeSwitcher.addEventListener("click", function() {
+    if (mode === "dark") {
+        mode = "light";
+        main.setAttribute("class", "light");
+    } else {
+        mode = "dark";
+        main.setAttribute("class", "dark");
+    }
+});
