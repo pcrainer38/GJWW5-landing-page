2b9dc3645b302852c3a94dcdcfcc9072d2fd42d4
pcrainer38 <pammie_78704@yahoo.com>
2024-08-27 12:08:56 -0500

    added countdown clock

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,33 @@ const setHalfVolume = () =>
     }
 
 setHalfVolume();
+
+// set date we're counting down to
+let countDownDate = new Date("Jan 16, 2025").getTime();
+
+// update count every 1 second
+let x = setInterval(function() {
+
+    // get today's date and time
+    let now = new Date();
+
+    // find distance between now and the count down date
+    let distance = countDownDate - now;
+
+    //time calculation for days
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let  hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    // display the result in the element with id='demo'
+    document.getElementById("demo").innerHTML = days + " days " + hours + " h " + minutes + " mins";
+
+    // if the count down is finished, write some text
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "It's Time!!"
+    }
+}, 1000)
+
+setInterval();
