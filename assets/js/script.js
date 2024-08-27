43ee02e3c512c1b443a78175bfa6c6e68b04e1bb
pcrainer38 <pammie_78704@yahoo.com>
2024-08-27 13:15:02 -0500

    adjusted font-size for countdown clock @media and updated footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,7 +76,6 @@ function searchApi() {
             temperature = Math.floor((temperature - 273) * 1.8 + 32);
             console.log(temperature);
             currWeatherEl.append(temperature);
-            // displayCurrentWeather(temperature);
 
         })
 
@@ -90,42 +89,3 @@ function searchApi() {
 
 searchApi();
 
-// function displayCurrentWeather(temperature) {
-//     let resultsCard = document.createElement('div');
-//     let temperatureEl = document.createElement('h2');
-//     temperatureEl = setAttribute('class', 'card-content');
-//     temperatureEl.textContent = `Current Cancun Temp: ${temperature} F`;
-//     resultsCard.setAttribute('class', 'results-card');
-//     resultsCard.append(temperatureEl);
-//     currWeatherEl.innerHTML = '';
-//     currWeatherEl.append(temperatureEl);
-
-    // let resultsCard = document.createElement('div');
-    // let cardBody = document.createElement('div');
-    // let temperatureEl = document.createElement('p');
-    // let windEl = document.createElement('p');
-    // let humidityEl = document.createElement('p');
-    // let maxTempEl = document.createElement('p');
-    // let minTempEl = document.createElement('p');
-
-    // temperatureEl.setAttribute('class', 'card-content');
-    // temperatureEl.textContent = `Current Cancun Temp: ${temperature} F`;
-    // windEl.setAttribute('class', 'card-content');
-    // windEl.textContent = `Wind: ${wind} mph`;
-    // humidityEl.setAttribute('class', 'card-content');
-    // humidityEl.textContent = `Humidity: ${humidity}%`;
-    // maxTempEl.setAttribute('class', 'card-content');
-    // maxTempEl.textContent = `Max temp: ${maxTemp} F`;
-    // minTempEl.setAttribute('class', 'card-content');
-    // minTempEl.textContent = `Min temp: ${minTemp} F`;
-
-    // resultsCard.setAttribute('class', 'results-card');
-    // cardBody.setAttribute('class', 'card-body');
-    // resultsCard.append(cardBody);
-    // cardBody.append(temperatureEl);
-    // console.log(cardBody);
-    // currWeatherEl.innerHTML = '';
-    // currWeatherEl.append(resultsCard);
-// }
-
-// displayCurrentWeather();
