f560e776356d76d12dcc1e51cb79f988ab57aea1
pcrainer38 <pammie_78704@yahoo.com>
2024-08-27 13:10:24 -0500

    added temperature and adjusted font-size in @media

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,4 +49,83 @@ let x = setInterval(function() {
     }
 }, 1000)
 
-setInterval();
+// Current temp
+
+let currWeatherEl = document.querySelector('#weather');
+
+var apiKey = 'b25ce75b259c4e61caf9ce00a1f90876';
+
+
+function searchApi() {
+    let wxQueryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=cancun&appid=' + apiKey;
+
+
+    fetch(wxQueryUrl)
+        .then (function (response) {
+            console.log(response);
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('NETWORK RESPONSE ERROR')
+            }
+        })
+        .then(function (data) {
+            console.log('data:', data);
+
+            let temperature = data.main.temp;
+            temperature = Math.floor((temperature - 273) * 1.8 + 32);
+            console.log(temperature);
+            currWeatherEl.append(temperature);
+            // displayCurrentWeather(temperature);
+
+        })
+
+        .catch(function(error) {
+            console.error(error);
+        })
+
+        
+
+}
+
+searchApi();
+
+// function displayCurrentWeather(temperature) {
+//     let resultsCard = document.createElement('div');
+//     let temperatureEl = document.createElement('h2');
+//     temperatureEl = setAttribute('class', 'card-content');
+//     temperatureEl.textContent = `Current Cancun Temp: ${temperature} F`;
+//     resultsCard.setAttribute('class', 'results-card');
+//     resultsCard.append(temperatureEl);
+//     currWeatherEl.innerHTML = '';
+//     currWeatherEl.append(temperatureEl);
+
+    // let resultsCard = document.createElement('div');
+    // let cardBody = document.createElement('div');
+    // let temperatureEl = document.createElement('p');
+    // let windEl = document.createElement('p');
+    // let humidityEl = document.createElement('p');
+    // let maxTempEl = document.createElement('p');
+    // let minTempEl = document.createElement('p');
+
+    // temperatureEl.setAttribute('class', 'card-content');
+    // temperatureEl.textContent = `Current Cancun Temp: ${temperature} F`;
+    // windEl.setAttribute('class', 'card-content');
+    // windEl.textContent = `Wind: ${wind} mph`;
+    // humidityEl.setAttribute('class', 'card-content');
+    // humidityEl.textContent = `Humidity: ${humidity}%`;
+    // maxTempEl.setAttribute('class', 'card-content');
+    // maxTempEl.textContent = `Max temp: ${maxTemp} F`;
+    // minTempEl.setAttribute('class', 'card-content');
+    // minTempEl.textContent = `Min temp: ${minTemp} F`;
+
+    // resultsCard.setAttribute('class', 'results-card');
+    // cardBody.setAttribute('class', 'card-body');
+    // resultsCard.append(cardBody);
+    // cardBody.append(temperatureEl);
+    // console.log(cardBody);
+    // currWeatherEl.innerHTML = '';
+    // currWeatherEl.append(resultsCard);
+// }
+
+// displayCurrentWeather();
